7578b9856d35bb0b32067181e0d1128b633ab9c7
James Fluckiger <jamesafluke@gmail.com>
2022-06-24 06:56:05 -0600

    Added service principle info in Terraform Cloud

--- maintest.tf
+++ maintest.tf
@@ -1,15 +1,13 @@
 terraform {
-  cloud {
+    cloud {
     organization = "jamesafluke1"
 
     workspaces {
       name = "Terraform_Module_ACE_workspace"
     }
   }
-}
 
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -22,12 +20,11 @@ provider "azurerm" {
   features {}
 
   # CDW sub
-  subscription_id = "3c2067eb-14d4-42da-83ca-73316e5fce7a"
-  tenant_id       = "c022673d-2b5c-406e-bb6a-d9e40ee64bc9"
+  # subscription_id = "3c2067eb-14d4-42da-83ca-73316e5fce7a"
+  # client_id       = "ZgT8Q~uB3toXh_b06PFKxOZA33hQFVKPZKpMGa_Y"
+  # client_secret   = var.client_secret
+  # tenant_id       = "c022673d-2b5c-406e-bb6a-d9e40ee64bc9"
 
-  # Personal sub
-  #   subscription_id = "74c2e3ac-be44-401a-acea-768ddd167a5f"
-  #   tenant_id       = "c74940ec-9df1-429e-9e18-5ee10c445e4f"
 }
 
 
