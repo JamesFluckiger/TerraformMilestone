34d44cff89e3a5bc76656a6fc06de39473306c8a
Jamesafluke <jamesafluke@gmail.com>
2022-06-25 20:47:18 -0600

    fmt

--- maintest.tf
+++ maintest.tf
@@ -1,5 +1,5 @@
 variable "sa_key" {
-  type = string
+  type      = string
   sensitive = "true"
 
 }
@@ -28,7 +28,7 @@ provider "azurerm" {
   subscription_id = "3c2067eb-14d4-42da-83ca-73316e5fce7a"
   # client_id       = "ZgT8Q~uB3toXh_b06PFKxOZA33hQFVKPZKpMGa_Y"
   # client_secret   = var.client_secret
-  tenant_id       = "c022673d-2b5c-406e-bb6a-d9e40ee64bc9"
+  tenant_id = "c022673d-2b5c-406e-bb6a-d9e40ee64bc9"
 
 }
 
